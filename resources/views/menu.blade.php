4341eb06bcb840f1849845ac4e11534c06053dd8
BeknazarovTemur <imtemurb@gmail.com>
2024-02-01 15:53:48 +0500

    added blade files

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,10 @@
+@section('content')
+
+<div class="container" id="menu">
+    <a href="#" class="btn btn-default">Parts</a> &nbsp; 
+    <a href="#" class="btn btn-default">Requisitions</a> &nbsp; 
+    <a href="#" class="btn btn-default">Orders</a> &nbsp; 
+    <a href="#" class="btn btn-default">Pick Tickets</a> &nbsp; 
+</div>
+
+@endsection
